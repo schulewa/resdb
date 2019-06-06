1f7fd2b0c720b995d9e6b58412753fe3159bf80f
Adrian Schulewitz <adrian.schulewitz@gmail.com>
2019-06-06 12:20:54 +1000

    RESDB-0002: add refdata module

---
+++ src/app/refdata/refdata.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReferenceDataRoutingModule } from './refdata-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReferenceDataRoutingModule
+  ]
+})
+export class ReferenceDataModule { }

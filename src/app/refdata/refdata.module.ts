a8393c68bf62bf8a820c21acaf00b5f17c271ae7
Adrian Schulewitz <adrian.schulewitz@gmail.com>
2019-06-06 14:55:32 +1000

    RESDB-0003: add simple-refdata component

--- src/app/refdata/refdata.module.ts
+++ src/app/refdata/refdata.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ReferenceDataRoutingModule } from './refdata-routing.module';
 import { SimpleRefdataComponent } from './simple-refdata/simple-refdata.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [SimpleRefdataComponent],
   imports: [
     CommonModule,
+    CoreModule,
     ReferenceDataRoutingModule
   ]
 })

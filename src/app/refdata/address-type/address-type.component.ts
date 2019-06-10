a75c822d3ea04cdb9303c09a29ff55f629b59806
Adrian Schulewitz <adrian.schulewitz@gmail.com>
2019-06-10 13:15:17 +1000

    RESDB-0004: tidy up address type and clear error on successful save

--- src/app/refdata/address-type/address-type.component.ts
+++ src/app/refdata/address-type/address-type.component.ts
@@ -51,44 +51,46 @@ export class AddressTypeComponent extends BaseNameComponent<AddressType> impleme
   }
 
   saveChanges() {
-    console.log('AddressTypeComponent.saveChanges - entry - number of rows = ' + this.rowData.length);
     // save only new and modified rows
     const toBeSaved = this.rowData.filter(row => row.action != null);
-    const results: AddressType[] = [];
 
     if (toBeSaved) {
       for (const addressType of toBeSaved) {
         if (DataAction.Add === addressType.action) {
+          this.operationMessage = CoreOperationsMessages.ADD_ADDRESS_TYPE;
           this.enrichAuditData(addressType);
           this.addressTypeService.add(addressType).subscribe(
             data => {
             console.log('Address type ' + addressType.action + 'ed - typeof result=' + typeof data);
+            this.httpError = null;
             this.updateGrid(data);
           },
             err => {
               console.error('AddressTypeComponent.add: err="' + err);
               this.httpError = err;
-              this.operationMessage = CoreOperationsMessages.ADD_ADDRESS_TYPE;
           });
         } else if (DataAction.Update === addressType.action) {
+          this.operationMessage = CoreOperationsMessages.UPDATE_ADDRESS_TYPE;
           addressType.status = DataStatus.Amend;
           this.addressTypeService.update(addressType).subscribe(
             data => {
             console.log('Address type ' + addressType.action + 'ed - result=' + data);
+            this.httpError = null;
             this.updateGrid(data);
           },
             err => {
               console.error('AddressTypeComponent.update: err="' + err);
               this.httpError = err;
-              this.operationMessage = CoreOperationsMessages.UPDATE_ADDRESS_TYPE;
             }
           );
 
         } else if (DataAction.Delete === addressType.action) {
+          this.operationMessage = CoreOperationsMessages.DELETE_ADDRESS_TYPE;
           addressType.status = DataStatus.Delete;
           this.addressTypeService.delete(addressType).subscribe(
             data => {
               console.log('Address type ' + addressType.action + 'ed - result=' + data);
+              this.httpError = null;
               const remainingRows: IAuditedNameDataType[] = this.rowData.filter(r => (this.liveStatuses.includes(r.status)));
               // this.updateGrid(remainingRows);
               this.gridApi.setRowData(remainingRows);
@@ -97,7 +99,6 @@ export class AddressTypeComponent extends BaseNameComponent<AddressType> impleme
             err => {
               console.error('AddressTypeComponent.delete: err="' + err);
               this.httpError = err;
-              this.operationMessage = CoreOperationsMessages.DELETE_ADDRESS_TYPE;
             }
           );
         }

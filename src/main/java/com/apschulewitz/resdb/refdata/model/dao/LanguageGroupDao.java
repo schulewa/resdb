519774372056fc66acdd33ab6a324ca0b3003226
Adrian Schulewitz <adrian.schulewitz@gmail.com>
2019-06-13 15:43:25 +1000

    RESDB-0013: filter out inactive entries when finding all

--- src/main/java/com/apschulewitz/resdb/refdata/model/dao/LanguageGroupDao.java
+++ src/main/java/com/apschulewitz/resdb/refdata/model/dao/LanguageGroupDao.java
@@ -1,9 +1,13 @@
 package com.apschulewitz.resdb.refdata.model.dao;
 
+import com.apschulewitz.resdb.common.model.entity.VersionStatus;
 import com.apschulewitz.resdb.refdata.model.entity.LanguageGroup;
+import com.apschulewitz.resdb.refdata.model.entity.MeasureType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by adrianschulewitz on 01/10/2016.
@@ -11,5 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Repository
 public interface LanguageGroupDao extends CrudRepository<LanguageGroup, Long> {
+
+  List<LanguageGroup> findByStatusIn(List<VersionStatus> livestatuses);
 
 }
